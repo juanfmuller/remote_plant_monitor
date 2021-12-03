de36a93cc2528a00c17258d3780b0c03f2519786
INFINITELOGIN <92061206+INFINITELOGIN@users.noreply.github.com>
2021-12-03 05:59:45 -0700

    Update both_sensors.py

--- embedded_software/both_sensors.py
+++ embedded_software/both_sensors.py
@@ -10,7 +10,7 @@ from time import sleep
  
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
- 
+
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
@@ -18,19 +18,21 @@ i2c = busio.I2C(board.SCL, board.SDA)
 from adafruit_ads1x15.analog_in import AnalogIn
 ads = ADS.ADS1015(i2c)
 chan0 = AnalogIn(ads, ADS.P0)
-chan1 = AnalogIn(ads, ADS.P1)
- 
+chan1 = AnalogIn(ads, ADS.P1) 
+
+percentage = (chan0.voltage / 3.3) * 100
+
 def read_temp_raw():
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
- 
+
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
-        lines = read_temp_raw()
+        lines = read_temp_raw()  
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
@@ -42,8 +44,8 @@ def read_temp():
     
 while True:
     with open('moist_values.txt', 'a') as f:
-        print(math.trunc(chan0.voltage), file=f)
-        print('Humidity:',math.trunc(chan0.voltage))
+        print(math.trunc(percentage), file=f)
+        print('Humidity:',math.trunc(percentage)) 
         with open('temp_values.txt', 'a') as f:
             print(read_temp(), file=f)
             print('Temperature: ', read_temp())	
